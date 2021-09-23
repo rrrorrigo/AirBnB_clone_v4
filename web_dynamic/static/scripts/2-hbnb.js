cc1384fe4ac0c5d16160d3cee19a1ad4af758f74
rrrorrigo <rodrigobriones54@gmail.com>
2021-09-23 18:24:18 -0300

    testing

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -11,13 +11,14 @@ window.addEventListener('DOMContentLoaded', (e) => {
     }
     $('.amenities > h4').text(list.join(', '));
   });
+  const request = require('request');
   request('http://d701e46f5948.d79346c3.hbtn-cod.io:5001/api/v1/status/', function (error, response, body) {
     try {
       const json = JSON.parse(body);
       if (data.status === 'OK') {
         $('div#api_status').addClass('available');
       }
-    } catch { } finally {
+    } catch {
       $('div#api_status').removeClass('available');
     };
   });
