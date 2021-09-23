5028b4b6a0bcf06ddbec889bd26178bc9e1e29d9
rrrorrigo <rodrigobriones54@gmail.com>
2021-09-23 16:08:00 -0300

    testing

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,12 +1,13 @@
 window.addEventListener('DOMContentLoaded', (e ) => {
-  /* let checked = []; */
-  const id = $('input').attr('[data-id]');
+  /* let checked = [];
+  $("input[name=ctrl_CustomerType]:checked").attr('xmlvalue'); */
+  const id = $('input[data-id=:amenity_id]:checked').attr('data-id');
   alert(id);
-  $('[data-id="' + id + '"] input:checkbox').change(function () {
+  $('[data-id=""] input:checkbox').change(function () {
     let checked = [];
     alert("entro")
     $('[data-id="' + id + '"] input:checkbox:checked').each(function () {
-        checked.push($(this).attr('amenity_name'));
+        checked.push($(this).attr('data-name'));
         alert("identifico el click");
     });
     $('.amenities > h4').text(checked.join(', '));
