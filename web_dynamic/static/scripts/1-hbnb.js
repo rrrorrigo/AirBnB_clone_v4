b605e1c61d6ba63f5f1a9f708f92075a708114fe
rrrorrigo <rodrigobriones54@gmail.com>
2021-09-23 16:23:27 -0300

    testing

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,23 +1,6 @@
 window.addEventListener('DOMContentLoaded', (e) => {
-  /* let checked = [];
-  $("input[name=ctrl_CustomerType]:checked").attr('xmlvalue'); */
-  const id = []
-  $('input[data-id]').each(function () {
-    id.append(this.attr('data-id'));
-    alert(id);
+  $('input:checkbox').on('change', function() {
+    alert(this.attr('data-id'));
+    alert(this.attr('data-name'));
   });
-  $('input[data-id=:amenity_id] input:checkbox').change(function () {
-    let checked = [];
-    alert("entro")
-    $('[data-id="' + id + '"] input:checkbox:checked').each(function () {
-        checked.push($(this).attr('data-name'));
-        alert("identifico el click");
-    });
-    $('.amenities > h4').text(checked.join(', '));
-  });
-  /* $.each($('input[data-id=":amenity_id"]:checked'), function () {
-    checked.push($(this).val());
-    alert(checked);
-    $('.amenities > h4').text(checked.join(', '))
-  }); */
 });
