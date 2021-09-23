44f7acaa34b16f2b1f483855f3a44a105380fad0
rrrorrigo <rodrigobriones54@gmail.com>
2021-09-23 15:40:54 -0300

    testing

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
-  $('.popover').focus(function () {
-    let checked = [];
-    $.each($('input[data-id=":amenity_id"]:checked'), function () {
-      checked.push($(this).val());
-      $('.amenities > h4').text(checked.join(', '));
-    });
+  let checked = [];
+  $.each($('input[data-id=":amenity_id"]:checked'), function () {
+    checked.push($(this).val());
+    alert(checked);
+    $('.amenities > h4').text(checked.join(', '))
   });
 });
