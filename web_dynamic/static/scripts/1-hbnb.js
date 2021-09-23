389c8acbeff5054eaf383102ce39e1084bd9fde6
rrrorrigo <rodrigobriones54@gmail.com>
2021-09-23 14:29:23 -0300

    trying to fix

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
-  let checked = [];
-  $.each($('input[data-id=":amenity_id"]:checked'), function () {
-    checked.push($(this).val());
-    alert(checked);
-    $('.amenities > h4').text(checked.join(', '))
+  $('.popover').focus(function () {
+    let checked = [];
+    $.each($('input[data-id=":amenity_id"]:checked'), function () {
+      checked.push($(this).val());
+      alert(checked);
+      $('.amenities > h4').text(checked.join(', '));
+    });
   });
 });
