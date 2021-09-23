ea3a8bffd4ff900d5da8fdd6706e08377b67fd72
rrrorrigo <rodrigobriones54@gmail.com>
2021-09-23 14:06:43 -0300

    upload new files

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  let checked = [];
+  $.each($('input[data-id=":amenity_id"]:checked'), function () {
+    checked.push($(this).val());
+  });
+  $('.amenities > h4').text(checked.join(', '))
+});
