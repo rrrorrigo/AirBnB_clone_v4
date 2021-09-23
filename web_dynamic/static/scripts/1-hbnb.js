a47c576e3cb7e7c854f54b15c74d308d0f5abe37
rrrorrigo <rodrigobriones54@gmail.com>
2021-09-23 16:53:14 -0300

    testing

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,14 @@
 window.addEventListener('DOMContentLoaded', (e) => {
-  let list = {};
+  let list = [];
   $('input:checkbox').change(function() {
     if ($(this).is(":checked")) {
       const name = ($(this).attr('data-name'));
-      list.name = name;
+      list.push(name);
     } else {
       const name = ($(this).attr('data-name'));
-      list.pop(name);
+      const i = list.indexOf(name);
+      list.splice(i, i);
     }
-    $('.amenities > h4').text(list)
+    $('.amenities > h4').text(list.join(', '))
   });
 });
