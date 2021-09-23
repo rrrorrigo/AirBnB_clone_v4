da6207d39964509631bbd362f01832e6de26890b
rrrorrigo <rodrigobriones54@gmail.com>
2021-09-23 19:10:30 -0300

    fixed url

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', (e) => {
+  let list = [];
+  $('input:checkbox').change(function() {
+    if ($(this).is(":checked")) {
+      const name = ($(this).attr('data-name'));
+      list.push(name);
+    } else {
+      const name = ($(this).attr('data-name'));
+      const i = list.indexOf(name);
+      list.splice(i, 1);
+    }
+    $('.amenities > h4').text(list.join(', '));
+  });
+  $.get('http://0.0.0.0:5001/api/v1/places_search/', function (data, status) {
+    if (status) {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+});
