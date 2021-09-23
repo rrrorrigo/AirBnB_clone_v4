248110e267df501b6638d225adaf316eadd34843
rrrorrigo <rodrigobriones54@gmail.com>
2021-09-23 16:31:15 -0300

    testing

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', (e) => {
   $('input:checkbox').change(function() {
-    alert($(this).attr('data-id'));
+    $(this).attr('data-id');
     alert($(this).attr('data-name'));
   });
 });
