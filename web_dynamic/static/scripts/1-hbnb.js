5d295d9e48fa146614dd944908d422e8ac0bb9a6
rrrorrigo <rodrigobriones54@gmail.com>
2021-09-23 15:54:10 -0300

    testing

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,9 +1,17 @@
 window.addEventListener('DOMContentLoaded', (e  ) => {
-  let checked = [];
-  alert("8==D importado bien");
-  $.each($('input[data-id=":amenity_id"]:checked'), function () {
+  /* let checked = []; */
+  $('[data-id=":amenity_id"] input:checkbox').change(function () {
+    let checked = [];
+    alert("entro")
+    $('[data-id=":amenity_id"] input:checkbox:checked').each(function () {
+        checked.push($(this).attr('amenity_name'));
+        alert("identifico el click");
+    });
+    $('.amenities > h4').text(checked.join(', '));
+  });
+  /* $.each($('input[data-id=":amenity_id"]:checked'), function () {
     checked.push($(this).val());
     alert(checked);
     $('.amenities > h4').text(checked.join(', '))
-  });
+  }); */
 });
