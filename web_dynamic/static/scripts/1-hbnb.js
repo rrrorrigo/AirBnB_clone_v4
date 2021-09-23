024ee2c08408faeee77cb621184a7d39abce6b71
rrrorrigo <rodrigobriones54@gmail.com>
2021-09-23 16:28:44 -0300

    testing

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', (e) => {
   alert("entrando al script...");
-  $('.popover').on('change', function() {
-    alert(this.attr('data-id'));
-    alert(this.attr('data-name'));
+  $('input:checkbox').change(function() {
+    alert($(this).attr('data-id'));
+    alert($(this).attr('data-name'));
   });
 });
