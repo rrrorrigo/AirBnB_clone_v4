f95cd8ab2e94523ec60aceab624d240028bce8b7
rrrorrigo <rodrigobriones54@gmail.com>
2021-09-23 15:38:08 -0300

    added import

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
     let checked = [];
     $.each($('input[data-id=":amenity_id"]:checked'), function () {
       checked.push($(this).val());
-      alert(checked);
       $('.amenities > h4').text(checked.join(', '));
     });
   });
